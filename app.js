2e00832995fa3cceecb38ecf3b3989a1c8bd18b3
Evgeny Galitsky <e.galitsky@bk.ru>
2022-11-01 00:12:02 +0300

    refactor: change/add some classnames using BEM to rule 2.1(advanced)

--- app.js
+++ app.js
@@ -8,10 +8,10 @@
 
 // Event handling, user interaction is what starts the code execution.
 
-var taskInput=document.getElementById("new-task");//Add a new task.
+var taskInput=document.getElementById("add-task__id");//Add a new task.
 var addButton=document.getElementsByTagName("button")[0];//first button
-var incompleteTaskHolder=document.getElementById("todo-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.getElementById("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.getElementById("todo-task__id");//ul of #incompleteTasks
+var completedTasksHolder=document.getElementById("complete-task__id");//completed-tasks
 
 
 //New task list item
